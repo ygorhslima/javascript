6f418e18b150f51c4b5297dfda3ad9205dd1119c
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-07-27 16:27:47 -0300

    Create aula055-set.js

---
+++ aula055-set.js
@@ -0,0 +1,4 @@
+const utensilios = new Set(['Cadeira','ventilador','mesa','cama','fogão','geladeira','notebook'])
+utensilios.add("panela de pressão")
+utensilios.add("Cadeira") /*NÃO ENTRA*/
+console.log(utensilios)
